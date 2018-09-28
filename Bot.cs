cd25f44daed85537bd9b38491751ba525f4bba89
James Platt <James.Platt@Neptune.com>
2018-09-28 15:14:56 +0100

    Kinda working but trash bot

---
+++ Bot.cs
@@ -0,0 +1,67 @@
+﻿using BotInterface.Bot;
+using BotInterface.Game;
+using System;
+
+namespace SuperCoolBotWhatIDidMake
+{
+    public class Bot : IBot
+    {
+        const int PlayDynamiteAtRoundValue = 2;
+
+        private readonly GameStateAnalyser GameStateAnalyser = new GameStateAnalyser();
+
+        public Move MakeMove(Gamestate gamestate)
+        {
+            GameStateAnalyser.RoundIndex++;
+            if (IsItWorthPlayingDynamite(gamestate))
+            {
+                if(GameStateAnalyser.DynamiteAvailable())
+                {
+                    return PlayDynamite();
+                }
+                if(GameStateAnalyser.OpponentHasDynamiteAvailable(gamestate))
+                {
+                    return PlayWater();
+                }
+            }
+            return PlayRandomStandard();
+        }
+
+        private Move PlayDynamite()
+        {
+            GameStateAnalyser.RemainingDynamite--;
+            return Move.D;
+        }
+
+        private Move PlayWater()
+        {
+            return Move.W;
+        }
+
+        private Move PlayRandomStandard()
+        {
+            var rand = new Random();
+            switch (rand.Next(0,3))
+            {
+                case 1:
+                    return Move.R;
+                case 2:
+                    return Move.P;
+                default:
+                    return Move.S;
+            }
+        }
+
+        private bool IsItWorthPlayingDynamite(Gamestate gamestate)
+        {
+            return GameStateAnalyser.GetRoundValue(gamestate) >= PlayDynamiteAtRoundValue;
+        }
+
+        private bool TossACoin()
+        {
+            Random rnd = new Random();
+            return (rnd.Next(0, 100) % 2 == 0);
+        }
+
+    }
+}
---
+++ GameStateAnalyser.cs
@@ -0,0 +1,55 @@
+﻿using BotInterface.Bot;
+using BotInterface.Game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperCoolBotWhatIDidMake
+{
+    class GameStateAnalyser
+    {
+        public int RemainingDynamite = 100;
+        public int RoundIndex { get; set; }
+        private int _roundValue = 1;
+
+        public bool DynamiteAvailable()
+        {
+            return RemainingDynamite > 0;
+        }
+
+        public int GetRoundValue(Gamestate gamestate)
+        {
+            if(RoundIndex > 1)
+            {
+                CalculateRoundValue(gamestate);
+            }
+            return _roundValue;
+        }
+
+        public bool OpponentHasDynamiteAvailable(Gamestate gamestate)
+        {
+            int dynamiteUsed = 0;
+            foreach(Round round in gamestate.GetRounds())
+            {
+                if (round.GetP2() == Move.D)
+                {
+                    dynamiteUsed++;
+                }
+            }
+            return dynamiteUsed > 100;
+        }
+
+        private void CalculateRoundValue(Gamestate gamestate)
+        {
+            var round = gamestate.GetRounds()[RoundIndex - 2];
+            if (round.GetP1() == round.GetP2())
+            {
+                _roundValue++;
+            }
+            else
+            {
+                _roundValue = 1;
+            }
+        }
+    }
+}
